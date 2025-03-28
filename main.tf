fd73102aad17193fe1374811fbeb458a1c89f2c2
Shreyas Patil <shreyaspatil4780@gmail.com>
2025-03-28 11:49:46 +0530

    Solved backslash issue in  main.tf

--- main.tf
+++ main.tf
@@ -95,8 +95,8 @@ resource "aws_instance" "flask_app" {
   subnet_id              = data.aws_subnet.selected_subnet.id
   vpc_security_group_ids = [aws_security_group.flask_app_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  user_data              = templatefile("\${path.module}/user_data.tftpl", {
-    app_code = file("\${path.module}/app.py")
+  user_data = templatefile("\${path.module}/user_data.tftpl", {
+  app_code = file("\${path.module}/app.py")
   })
   
   tags = {
